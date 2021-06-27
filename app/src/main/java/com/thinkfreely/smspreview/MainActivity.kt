9f6afcc1e60ba02edd02a4b70bd8bf341d90981b
Neil Horman <nhorman@gmail.com>
2021-06-27 16:24:42 -0400

    initial import

---
+++ app/src/main/java/com/thinkfreely/smspreview/MainActivity.kt
@@ -0,0 +1,21 @@
+package com.thinkfreely.smspreview
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.content.IntentFilter
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import androidx.localbroadcastmanager.content.LocalBroadcastManager
+import java.util.jar.Manifest
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        ActivityCompat.requestPermissions(this@MainActivity, arrayOf(android.Manifest.permission.RECEIVE_SMS),1)
+    }
+}
