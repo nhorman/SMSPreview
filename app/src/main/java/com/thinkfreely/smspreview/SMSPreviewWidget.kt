ddde6d9b85f7310b3e2c0530a2453766a73bf6fb
Neil Horman <nhorman@gmail.com>
2021-06-29 16:40:12 -0400

    saving work

--- app/src/main/java/com/thinkfreely/smspreview/SMSPreviewWidget.kt
+++ app/src/main/java/com/thinkfreely/smspreview/SMSPreviewWidget.kt
@@ -2,6 +2,7 @@ package com.thinkfreely.smspreview
 
 import android.appwidget.AppWidgetManager
 import android.appwidget.AppWidgetProvider
+import android.content.ComponentName
 import android.content.Context
 import android.content.Intent
 import android.provider.Telephony
@@ -32,15 +33,21 @@ class SMSPreviewWidget : AppWidgetProvider() {
     }
 
     override fun onReceive(context: Context, intent: Intent) {
-        super.onReceive(context, intent)
         // This method is called when the BroadcastReceiver is receiving an Intent broadcast.
         if (intent.action != "android.provider.Telephony.SMS_RECEIVED") {
+                return super.onReceive(context, intent)
                 return
         }
         val smsMessages = Telephony.Sms.Intents.getMessagesFromIntent(intent)
+        val views = RemoteViews(context.packageName, R.layout.s_m_s_preview_widget)
         for (message in smsMessages) {
+            views.setTextViewText(R.id.appwidget_text, message.displayMessageBody)
             Log.i("SMSPreviewWidget", message.displayMessageBody + "From Widget")
         }
+        val widgetManager = AppWidgetManager.getInstance(context)
+        val currWidget = ComponentName(context, SMSPreviewWidget::class.java)
+        widgetManager?.updateAppWidget(currWidget, views)
+
     }
 }
 
